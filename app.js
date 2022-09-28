426a68fb734099230a5bf412ec729969f699d381
1emre <emredmrbs99@gmail.com>
2022-09-28 20:00:07 +0300

    Lesson 03

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
+
+//Connect Db
+mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
+  console.log('DB Connected Successfully');
+});
 
 //Templete Engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index',
-  });
-});
-
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: 'about',
-  });
-});
+//Routes
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
