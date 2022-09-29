656df6e335943f919be785de62afc0b06bfd2d15
1emre <emredmrbs99@gmail.com>
2022-09-29 12:45:14 +0300

    Giriş Sayfası ve Giriş İşlemleri

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,34 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 exports.createUser = async (req, res) => {
   try {
-    const category = await User.create(req.body);
+    const user = await User.create(req.body);
     res.status(201).json({
       status: 'success',
-      category,
+      user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body; //req gelen email ve password aldım
+
+    const user = await User.findOne({ email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          // USER SESSION
+          res.status(200).send('YOU ARE LOGGED IN');
+        }
+      });
+    }
   } catch (error) {
     res.status(400).json({
       status: 'fail',
