c563c88a1c7519ea3902ce412fa2299f5cf20c23
1emre <emredmrbs99@gmail.com>
2022-09-30 13:28:09 +0300

    Bildirim Mesajları

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -32,8 +32,9 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendMail = async (req, res) => {
-  // htmlden gelen body nin name alanlarının degerlerini alıyorum
-  const outputMessage = `
+  try {
+    // htmlden gelen body nin name alanlarının degerlerini alıyorum
+    const outputMessage = `
 
   <h1>Mail Details </h1>
   <ul>
@@ -44,31 +45,39 @@ exports.sendMail = async (req, res) => {
   <p>${req.body.message}</p>
   `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: 'emredmrbs99@gmail.com', // gmail account
-      pass: 'qfplpgsqupcnyurp', // gmail password -- uygulama şifrelerinden yeni şifre aldım
-    },
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'emredmrbs99@gmail.com', // gmail account
+        pass: 'qfplpgsqupcnyurp', // gmail password -- uygulama şifrelerinden yeni şifre aldım
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart EDU Contact Form" <emredmrbs99@gmail.com>', // sender address
-    to: 'emredmrbs99@gmail.com', // list of receivers
-    subject: 'Smart EDU Contact From New Message', // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <emredmrbs99@gmail.com>', // sender address
+      to: 'emredmrbs99@gmail.com', // list of receivers
+      subject: 'Smart EDU Contact From New Message', // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log('Message sent: %s', info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  res.status(200).redirect('/contact');
+    req.flash('success', 'We received your message successfully'); // contact templete redirect ediyorum ve bunu orda yakalamam
+    //lazım nasıl yakılıyorum app.js olusturdugum flasMessage değişkeniyle yakalıyorum.
+
+    res.status(200).redirect('/contact');
+  } catch (error) {
+    //req.flash('error', `Something happened! ${err}`);
+    req.flash('error', `Something happened!`);
+    res.status(400).redirect('/contact');
+  }
 };
